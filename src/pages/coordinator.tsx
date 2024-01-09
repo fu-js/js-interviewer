4bdd34a353f58225ee1b3ed13a18f177fd73526c
DAN3002 <dinhanh300229@gmail.com>
2024-01-10 02:58:26 +0700

    feat: init data and table for checked in candidate

--- src/pages/coordinator.tsx
+++ src/pages/coordinator.tsx
@@ -14,50 +14,20 @@ import {
 import { ChevronLeftIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
+const checkedInCandidates = [{
+  "id": 41,
+  "fullName": "Nguyễn Đức Thắng",
+  "department": {
+    "id": 1,
+    "name": "Ban Văn hóa"
   },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+  "phoneNumber": "945228637",
+  "interviewSlot": {
+    "id": 1,
+    "order": 0,
+    "slotTime": "7h00-9h00"
+  }
+}];
 
 export default function Coordinator() {
   return (
@@ -75,30 +45,37 @@ export default function Coordinator() {
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead className="w-[100px]">Invoice</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead>Method</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
+                  <TableHead className="w-[100px]">ID</TableHead>
+                  <TableHead>Full Name</TableHead>
+                  <TableHead>Department</TableHead>
+                  <TableHead>Slot Time</TableHead>
+                  <TableHead>Action</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {invoices.map((invoice) => (
-                  <TableRow key={invoice.invoice}>
-                    <TableCell className="font-medium">
-                      {invoice.invoice}
-                    </TableCell>
-                    <TableCell>{invoice.paymentStatus}</TableCell>
-                    <TableCell>{invoice.paymentMethod}</TableCell>
-                    <TableCell className="text-right">
-                      {invoice.totalAmount}
+                {checkedInCandidates.map((candidate) => (
+                  <TableRow key={candidate.id}>
+                    <TableCell>{candidate.id}</TableCell>
+                    <TableCell>{candidate.fullName}</TableCell>
+                    <TableCell>{candidate.department.name}</TableCell>
+                    <TableCell>{candidate.interviewSlot.slotTime}</TableCell>
+                    <TableCell>
+                      <Button className="mx-2">
+                          {/* <ChevronLeftIcon /> */}
+                          Assign
+                      </Button>
+                      <Button className="mx-2">
+                          {/* <ChevronLeftIcon /> */}
+                          View Profile
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
               <TableFooter>
                 <TableRow>
-                  <TableCell colSpan={3}>Total</TableCell>
-                  <TableCell className="text-right">$2,500.00</TableCell>
+                  <TableCell colSpan={4}>Total Waiting</TableCell>
+                  <TableCell className="text-right">{checkedInCandidates.length}</TableCell>
                 </TableRow>
               </TableFooter>
             </Table>
