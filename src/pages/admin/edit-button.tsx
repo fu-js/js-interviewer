c54293332c34a19efe0dd096e838d9310b224e46
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2024-01-10 08:38:06 +0700

    feat: update UI

--- src/pages/admin/edit-button.tsx
+++ src/pages/admin/edit-button.tsx
@@ -1,15 +1,15 @@
 import DecisionGroup from "@/components/custom/decision-group";
 import { Button } from "@/components/ui/button";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { useState } from "react";
 
 export default function ({ interviewee: candidateData }: { interviewee: any }) {
@@ -18,44 +18,42 @@ export default function ({ interviewee: candidateData }: { interviewee: any }) {
   const [candidate, setCandidate] = useState(candidateData);
 
   function edit(candidate: any): void {
-    
+
   }
 
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
+    <Dialog>
+      <DialogTrigger asChild>
         <Button variant="secondary">Edit</Button>
-      </DrawerTrigger>
-      <DrawerContent>
+      </DialogTrigger>
+      <DialogContent>
         <div className="mx-auto w-full max-w-xl">
-          <DrawerHeader>
-            <DrawerTitle>
+          <DialogHeader>
+            <DialogTitle>
               <p className="text-xl font-medium">Edit</p>
-            </DrawerTitle>
-            <DrawerDescription className="text-muted-foreground">
+            </DialogTitle>
+            <DialogDescription className="text-muted-foreground">
               {candidate?.fullName} - {candidate?.department.name}
-            </DrawerDescription>
-          </DrawerHeader>
-          <div className="grid gap-4 p-4">
-            <div className="flex gap-4 items-center py-4">
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
               <DecisionGroup
                 defaultValue={candidateData.decision}
                 onValueChange={(value) =>
                   setCandidate({ ...candidate, decision: value })
                 }
               />
-            </div>
           </div>
-          <DrawerFooter>
-            <DrawerClose asChild>
+          <DialogFooter>
+            <DialogClose asChild>
               <Button onClick={() => edit({ ...candidate })}>Submit</Button>
-            </DrawerClose>
-            <DrawerClose asChild>
+            </DialogClose>
+            <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
+            </DialogClose>
+          </DialogFooter>
         </div>
-      </DrawerContent>
-    </Drawer>
+      </DialogContent>
+    </Dialog>
   );
 }
