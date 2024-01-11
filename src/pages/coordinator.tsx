15710f568c8c3e8c4391f60e9c3c361b54a909f3
DAN3002 <dinhanh300229@gmail.com>
2024-01-11 11:05:03 +0700

    feat: update coordinator screen

--- src/pages/coordinator.tsx
+++ src/pages/coordinator.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import BackButton from "@/components/custom/back-button";
 import Layout from "@/components/custom/layout";
 import { ModeToggle } from "@/components/custom/mode-toggle";
@@ -13,6 +14,10 @@ import {
 } from "@/components/ui/table";
 import { PlusIcon, PersonIcon } from "@radix-ui/react-icons";
 import useFetch from "@/lib/useFetch";
+import { Dialog } from '@headlessui/react';
+import requestBackend from "@/lib/requestBackend";
+import { useToast } from "@/components/ui/use-toast";
+
 
 // let checkedInCandidates = [{
 //   "id": 41,
@@ -61,7 +66,54 @@ export default function Coordinator() {
     departmentId: [1, 2, 3, 4, 5],
   });
 
-  console.log(checkedInCandidates);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [selectedCandidate, setSelectedCandidate] = useState<any>({});
+  const [selectedDesk, setSelectedDesk] = useState<any>([]);
+
+  const { toast } = useToast();
+
+  const togglePopup = (candidate: any) => {
+    setIsPopupOpen(!isPopupOpen);
+    setSelectedCandidate(candidate);
+
+    const selectedDepartment = candidate.department.name;
+    const filteredDesk = interviewDesk.filter((desk: any) => selectedDepartment.toLowerCase().includes(desk.department.toLowerCase()));
+    setSelectedDesk(filteredDesk);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    // Handle your submission logic here
+    console.log("Selected Candidate: ", selectedCandidate);
+
+    // get selected desk
+    const selectedDeskId = event.target.elements[0].value;
+    console.log("Selected Desk: ", selectedDeskId)
+
+    // fetch to add candidate to desk
+    // PUT /interview-server/public/v1/coordinator/send-to-interview-desk
+    const assign = async (candidate: any) => {
+      const checkinRes = await requestBackend(
+        `/coordinator/send-to-interview-desk/?candidateIds=${candidate.id}&interviewDeskId=${selectedDeskId}`,
+        { method: "PUT" }
+      );
+      const status = checkinRes.status;
+      if (status === 200) {
+        toast({ title: "Assign successfully" });
+        reloadCheckedInCandidates();
+      } else {
+        toast({ title: "Assign failed" });
+      }
+    };
+
+
+    // assign selected desk to selected candidate
+    assign(selectedCandidate);
+
+    setIsPopupOpen(false);
+    setSelectedCandidate({});
+    setSelectedDesk([]);
+  };
 
   return (
     <Layout>
@@ -96,7 +148,7 @@ export default function Coordinator() {
                     <TableCell>{candidate.department.name}</TableCell>
                     <TableCell>{candidate.interviewSlot.slotTime}</TableCell>
                     <TableCell>
-                      <Button className="mx-2">
+                      <Button className="mx-2" onClick={() => togglePopup(candidate)}>
                           <PlusIcon />
                           Assign
                       </Button>
@@ -142,6 +194,28 @@ export default function Coordinator() {
             </Table>
           </div>
         </div>
+
+        {isPopupOpen && (
+          <Dialog open={isPopupOpen} onClose={() => setIsPopupOpen(false)} className={`fixed inset-0 z-10 overflow-y-auto`}>
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+
+            <div className={`relative bg-white dark:bg-gray-800 rounded max-w-sm mx-auto mt-10 p-8`}>
+              <Dialog.Title className="text-gray-900 dark:text-gray-100">Assign Candidate</Dialog.Title>
+              <form onSubmit={handleSubmit}>
+                <select className="border p-2 rounded w-full text-gray-900 dark:text-gray-100 dark:border-gray-700 dark:bg-gray-700">
+                  {/* Filter desk with same department.id with selectedCandidate */}
+                  
+                  {selectedDesk && selectedDesk.map((desk: any) => (
+                    <option key={desk.id} value={desk.id}>{desk.name}</option>
+                  ))}
+                </select>
+                <Button type="submit" className="mt-4 bg-blue-500 dark:bg-blue-700 text-white">
+                  Submit
+                </Button>
+              </form>
+            </div>
+        </Dialog>
+        )}
       </main>
     </Layout>
   );
